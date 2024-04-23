f2391d0f275b3f98a7391798f0136e2c51e86679
Vadim Mikheev <21930@mail.ru>
2024-04-23 12:01:53 +0300

    Поменял название константе в MovieQuizPresenterTests

--- MovieQuizTests/MovieQuizPresenterTests.swift
+++ MovieQuizTests/MovieQuizPresenterTests.swift
@@ -18,10 +18,10 @@ final class MovieQuizViewControllerMock: MovieQuizViewControllerProtocol {
 final class MovieQuizPresenterTests: XCTestCase {
     func testPresenterConvertModel() throws {
         let viewControllerMock = MovieQuizViewControllerMock()
-        let sut = MovieQuizPresenter(viewController: viewControllerMock)
+        let presenter = MovieQuizPresenter(viewController: viewControllerMock)
         let emptyData = Data()
         let question = QuizQuestion(image: emptyData, text: "Question Text", correctAnswer: true)
-        let viewModel = sut.convert(model: question)
+        let viewModel = presenter.convert(model: question)
         XCTAssertNotNil(viewModel.image)
         XCTAssertEqual(viewModel.question, "Question Text")
         XCTAssertEqual(viewModel.questionNumber, "1/10")
